e975b7b2662f407d848e50c12925566a46777061
Juan Faria <juanfariacrf@gmail.com>
2022-12-26 15:11:29 -0300

    Inserção de dados com o prepared statement

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,29 +3,42 @@ package application;
 import db.DB;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
         Connection connection = null;
-        Statement st = null;
-        ResultSet rs = null;
+        PreparedStatement st = null;
         try {
             connection = DB.getConnection();
-            st = connection.createStatement();
-            rs = st.executeQuery("SELECT * FROM department");
-            while (rs.next()) {
-                System.out.println("Id: " + rs.getInt("Id") + ", " + rs.getString("Name"));
+            st = connection.prepareStatement("INSERT INTO seller" +
+                    "(Name,Email,BirthDate,BaseSalary,DepartmentId)" +
+                    "VALUES (?,?,?,?,?)");
+            st.setString(1, "José Junior");
+            st.setString(2, "jose@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("25/12/2000").getTime()));
+            st.setDouble(4, 3000.00);
+            st.setInt(5, 1);
+
+            int result = st.executeUpdate();
+            if (result > 0) {
+                System.out.println("Done");
+            }
+            else {
+                System.out.println("Error");
             }
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
-        finally {
-            DB.closeResultSet(rs);
+        catch (ParseException e) {
+            e.printStackTrace();
+        } finally {
+
             DB.closeStatement(st);
             DB.closeConnection();
         }
