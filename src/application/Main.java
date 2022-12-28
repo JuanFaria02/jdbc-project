6345ef78013fc9e3f8af852f23a6ce4e433df526
Juan Faria <juanfariacrf@gmail.com>
2022-12-28 09:38:31 -0300

    Classe Department criada

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
+import model.entities.Department;
 
 import java.io.IOException;
 import java.sql.*;
@@ -11,41 +12,7 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        Connection connection = null;
-        Statement st = null;
-        try {
-            connection = DB.getConnection();
-
-            connection.setAutoCommit(false);
-
-            st = connection.createStatement();
-            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090.00 " +
-                            "WHERE DepartmentId = 1");
-            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090.00 " +
-                    "WHERE DepartmentId = 3");
-
-            connection.commit();
-            System.out.println("Rows1: "+rows1);
-            System.out.println("Rows2: "+rows2);
-
-        }
-        catch (SQLException e) {
-            try {
-                connection.rollback();
-                throw new DbException("Transaction rolled back! Error caused by: " + e.getMessage());
-            }
-            catch (SQLException e2) {
-                throw new DbException("Error trying to rollback! Error caused by: " + e2.getMessage());
-
-            }
-
-        }
-        finally {
-
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+        Department department = new Department(1, "Books");
+        System.out.println(department);
     }
 }
