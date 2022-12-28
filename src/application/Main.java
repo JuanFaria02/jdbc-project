2e83dc7276f165792ce3b965a28279efc014a0c0
Juan Faria <juanfariacrf@gmail.com>
2022-12-28 10:04:23 -0300

    SellerDaoJDBC e DaoFactory criados

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,7 @@ public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 2000.0, department);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
