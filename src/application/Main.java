dd05e2db7e0c5fb9b8a3728cadbbe7317cbf5068
Juan Faria <juanfariacrf@gmail.com>
2022-12-28 09:49:09 -0300

    Classe Seller criada

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,15 +4,18 @@ import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.io.IOException;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
-        System.out.println(department);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 2000.0, department);
+        System.out.println(seller);
     }
 }
